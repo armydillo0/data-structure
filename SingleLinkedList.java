4ce5ebd291858f2041bd0e011b2ad265535bbb84
clara chu <clarajchu@gmail.com>
2017-05-09 22:01:44 -0400

    Update SingleLinkedList.java

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -30,7 +30,7 @@ public class SingleLinkedList {
 			getLastNode(head).setNext(n);
 		}
 	}
-	
+	//find the node value
 	public int get(int index) {
 		Node n = head;
 		for(int i=0; i < index; i++)
@@ -38,7 +38,47 @@ public class SingleLinkedList {
 		return n.getValue();
 	}
 	
-	// Remove
+	//find the node via its index
+	public Node getNode(int inputIndex) {
+		Node n = head; //useless
+		for (n = head; n.getNext() != null; n = n.getNext()) {
+			if (findIndex(n) == inputIndex) return n;
+		}return n; //useless
+	}
+	
+	//find the node index
+	public int findIndex(Node nodey) {
+		int num = 0;
+		for (Node n = head; n.getNext() != nodey.getNext(); n = n.getNext()) {
+			num++;
+		}return num;
+	}
+	
+	// Remove at given index
+	public void remove(int index) {
+		if (index < 0 || index > findSize()) {
+			throw new IllegalArgumentException("Index out of bound.");
+		}else if (index == 0) {
+			head = head.getNext();
+		}else {
+			Node p = getNode(index - 1);
+			Node c = p.getNext();//kill target
+			Node n = c.getNext();
+			
+			p.setNext(n);
+			c.setNext(null);
+		}
+	}
+	
+	//get list size
+	public int findSize() {
+		if (head != null) {
+			int num = 1;
+			for (Node n = head; n.getNext() != null; n = n.getNext())
+				num++;
+			return num;
+		}else return 0;
+	}
 	
 	@Override
 	public String toString() {
@@ -59,8 +99,24 @@ public class SingleLinkedList {
 		list.put(2);
 		list.put(3);
 		list.put(4);
+		list.put(5);
+		list.put(6);
+		list.put(7);
+		list.put(8);
+		list.put(9);
+		list.put(10);
+		list.put(11);
+		list.put(12);
 		
-		System.out.println(list);;
+		System.out.println("ORIGINAL SIZE OF LIST:"+ list.findSize());
+		System.out.println(list);
+		
+		System.out.println();
+		//list.remove(index);
+		list.remove(0);
+		list.remove(6);
+		
+		System.out.println("CURRENT SIZE OF LIST: "+ list.findSize());
+		System.out.println(list);
 	}
-
 }
